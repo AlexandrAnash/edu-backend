a05f763b0563b30448c3f02c568f1ed47facb64b
AlexandrAnash <alexanash92@gmail.com>
2013-11-18 17:00:06 +0400

    HomeTask_5

--- public/index.php
+++ public/index.php
@@ -3,21 +3,7 @@
 require_once __DIR__ . "/../src/models/Router.php";
 
 if (isset($_GET["page"])) {
-    @$router = new Router($_GET["page"]);
-    $controllerName = $router->getController();
-    $actionName = $router->getAction();
-    $path = __DIR__ . "/../src/controllers/{$controllerName}.php";
-    if (file_exists($path)) {
-        require_once $path;
-        $controller = new $controllerName;
-        if (method_exists($controller, $actionName))
-            $controller->$actionName();
-        else
-            require_once __DIR__ . "/../src/views/error404.phtml";
-    } else
-        require_once __DIR__ . "/../src/views/error404.phtml";
-
-
+    $router = new Router($_GET["page"]);
 } else {
     require_once __DIR__ . "/../src/controllers/ProductController.php";
     $controller = new ProductController();
--- src/models/Router.php
+++ src/models/Router.php
@@ -7,7 +7,28 @@ class Router
 
     public function __construct($getPage)
     {
-        list($this->_controller, $this->_action) = explode('_',$getPage);
+        try {
+            if (!stristr($getPage, '_'))
+                throw new Exception("In the address bar should be the character '_'.");
+            list($this->_controller, $this->_action) = explode('_', $getPage);
+            $controllerName = $this->getController();
+            $actionName = $this->getAction();
+            $path = __DIR__ . "/../controllers/{$controllerName}.php";
+            if (file_exists($path)) {
+                require_once $path;
+                $controller = new $controllerName;
+                if (method_exists($controller, $actionName))
+                    $controller->$actionName();
+                else
+                    throw new Exception("The wrong address!");
+            } else
+                throw new Exception("The wrong address!");
+
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            require_once __DIR__ . "/../views/error404.phtml";
+
+        }
     }
 
     public function getController()
